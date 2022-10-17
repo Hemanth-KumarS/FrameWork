19afb23d307e6300d4e85a835faf72a6faa170e4
Andy <Andy@192.168.231.1>
2022-10-17 12:46:36 +0530

    committed FrameWork

---
+++ com.crm.FrameWorkExample/src/main/java/com/crm/FrameWorkExample/com/crm/FrameWorkExample/App.java
@@ -0,0 +1,13 @@
+package com.crm.FrameWorkExample.com.crm.FrameWorkExample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
